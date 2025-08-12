690d0fa17eb8e43339ffafadf2a96635ca7df3e0
Tanmay Karad <154725109+karad-tanmay@users.noreply.github.com>
2025-08-12 14:47:53 +0530

    assignment 2 done

--- Assignment-2/aggregates-grouping-ordering.sql
+++ Assignment-2/aggregates-grouping-ordering.sql
@@ -14,17 +14,11 @@ GROUP BY section.building;
 -- Find all departments that have at least one instructor, and list the names of the departments along with the number of instructors;   order the result in descending order of number of instructors. 
 SELECT instructor.dept_name, COUNT(DISTINCT instructor.ID) AS InstructorCount
 FROM instructor
-GROUP BY dept_name;
+GROUP BY dept_name
+ORDER BY InstructorCount DESC;
 
 -- For each student, compute the total credits they have successfully completed, i.e. total credits of courses they have taken, for which they have a non-null grade other than 'F'. Do NOT use the tot_credits attribute of student.
 SELECT student.name, SUM(course.credits) AS TotalCredits
 FROM student, takes, course
 WHERE student.ID = takes.ID AND takes.course_id = course.course_id AND takes.grade <> 'F'
 GROUP BY student.name;
-
-SELECT DISTINCT name, tot_cred
-from student;
-
-select * from course;
-select * from student;
-select * from takes;
--- Assignment-2/nested-subqueries.sql
+++ Assignment-2/nested-subqueries.sql
@@ -1,9 +1,13 @@
 -- Find the id and title of all courses which do not require any  prerequisites.
-SELECT course.course_id, course.title
-FROM course, prereq
-WHERE course.course_id = prereq.course_id AND prereq.prereq_id IS NULL;
+SELECT course_id, title
+FROM course
+WHERE course_id NOT IN (SELECT course_id from prereq);
 
 -- Find the names of students who have not taken any biology dept. courses.
-SELECT DISTINCT student.name
-from student, takes, course
-WHERE student.ID = takes.ID AND takes.course_id = course.course_id AND course.dept_name <> 'Biology';
+SELECT name
+FROM student
+WHERE ID NOT IN (
+	SELECT takes.ID
+    FROM takes, course
+    WHERE takes.course_id = course.course_id AND course.dept_name = 'Biology'
+);
